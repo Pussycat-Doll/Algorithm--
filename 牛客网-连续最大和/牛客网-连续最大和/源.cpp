efccd94d9a3dfd502340f8682f97ae37f60ab9ac
Pussycat-Doll <2633974012@qq.com>
2019-12-18 21:38:43 +0800

    2019/12/18

--- 牛客网-连续最大和/牛客网-连续最大和/源.cpp
+++ 牛客网-连续最大和/牛客网-连续最大和/源.cpp
@@ -15,72 +15,36 @@
 3 -1 2 1
 输出
 3*/
+
 #include<iostream>
 #include<vector>
-#include<stdlib.h>
+#include<algorithm>
 using namespace std;
-//int main()
-//{
-//	vector<int> l = {-1,-2,-3,-4,-5,-6};
-//	int num,x;
-//	cin >> num;
-//	while (num--)
-//	{
-//		cin >> x;
-//		l.push_back(x);
-//	}
-//	int max = -10000;
-//	int end = 0;
-//	int start = 0;
-//	int sum = 0;
-//	for (size_t i = 0; i < l.size(); ++i)
-//	{
-//		end = i;
-//		while (end < l.size())
-//		{
-//			start = i;
-//			while (start <= end)
-//			{
-//				sum += l[start];
-//				++start;
-//			}
-//			if (sum > max)
-//				max = sum;
-//			sum = 0;
-//			++end;
-//		}
-//	}
-//	cout << max << endl;
-//	system("pause");
-//	return 0;
-//}
-
-int Max(int a, int b)
+int max_sum(vector<int> ve)
 {
-	return a > b ? a : b;
-}
-int get_max(vector<int> l)
-{
-	int sum = l[0];
-	int max = l[0];
-	for (size_t i = 0; i < l.size(); ++i)
+	int Max = ve[0];
+	int sum = ve[0];
+	for (size_t i = 1; i < ve.size(); ++i)
 	{
-		sum = Max(sum + l[i], l[i]);//动态规划状态方程 
-		max = Max(sum, max);
+		sum = max(sum + ve[i], ve[i]);
+		Max = max(sum, Max);
 	}
-	return max;
+	return Max;
 }
 int main()
 {
-	vector<int> l;
-	int num, x;
-	cin >> num;
-	while (num--)
+	int length = 3;
+	while (cin >> length)
 	{
-		cin >> x;
-		l.push_back(x);
+		int num;
+		vector<int> ve;
+		while (length--)
+		{
+			cin >> num;
+			ve.push_back(num);
+		}
+		cout << max_sum(ve) << endl;
 	}
-	cout << get_max(l) << endl;
-	system("pause");     
+	system("pause");
 	return 0;
 }
